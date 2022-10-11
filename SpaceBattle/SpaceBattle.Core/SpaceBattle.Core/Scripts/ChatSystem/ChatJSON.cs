1197b7a775b311ec5b59ce711a2d7aff8140505f
Math23k5 <93868847+Math23k5@users.noreply.github.com>
2022-10-11 10:54:33 +0200

    Chatsystem NOT DONE
    Tried to add a chat system but need to build some functionality from a documented library

---
+++ SpaceBattle/SpaceBattle.Core/SpaceBattle.Core/Scripts/ChatSystem/ChatJSON.cs
@@ -0,0 +1,102 @@
+﻿using CosmosEngine;
+using CosmosEngine.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SpaceBattle
+{
+    internal class ChatJSON : GameBehaviour
+    {
+        //variabler til chatbox
+        public InputField input;
+        public TextField chatOutputText;
+
+        //URLs til at GET/POST chat
+        private string chatGETUrl = "INSERT URL";
+        private string chatPOSTUrl = "INSERT URL";
+
+        //JSON
+        private string chatJSONString;
+        //private JsonData      LitJson lib https://litjson.net/api/LitJson/JsonData/
+        public string chatOutput;
+        public string newText;
+        public List<Message> chatList = new List<Message>();
+        public string chatNewString;
+        int messageCount;
+
+        public string displayTextString = "";
+
+        protected override void Start()
+        {
+            //Task<GETChatJSON>();
+        }
+
+        //private IEnumerator GETChatJSON()
+        //{
+            //WWW chat = new WWW(chatGETUrl); LitJson lib
+            //yield return chat;
+        //    if (chat.error == null)
+        //    {
+
+        //        Debug.Log("Connection is good to: " + chatGETUrl);
+
+        //    }
+        //    else
+        //    {
+        //        Debug.Log("ERROR: " + chat.error);
+        //    }
+
+        //    chatJSONString = chat.text;
+        //    chatStringData = JsonMapper.ToObject(chatJSONString);
+
+        //    GetChatText();
+        //}
+
+        public void GetChatText()
+        {
+            string msgID;
+            string msgContent;
+            string msgUsername;
+
+            //for (int i = 0; i < chatStringData["messages"].Count; i++)
+            //{
+            //    msgID = chatStringData["messages"][i]["chat_message_id"].ToString();
+            //    msgContent = chatStringData["messages"][i]["msg_content"].ToString();
+            //    msgUsername = chatStringData["messages"][i]["username"].ToString();
+
+
+
+            //    //displayTextString = "User: " + msgUsername + "      " + "Message: " + msgContent
+
+
+            //    chatList.Add(new Message
+            //    {
+            //        messageId = msgID,
+            //        messageContent = msgContent,
+            //        messageUsername = msgUsername,
+            //    });
+            //    messageCount = i;
+
+            //}
+            //DisplayMessage();
+        }
+        //void DisplayMessage()
+        //{
+        //    chatOutputText.text = "";
+        //    IEnumerable<Message> sortedMessages =
+        //         from Message in chatList
+        //         select Message;
+
+        //    foreach (Message msg in sortedMessages)
+        //    {
+
+        //        chatNewString = chatNewString + msg.messageUsername + ": " + msg.messageContent + "\n";
+        //    }
+        //    chatOutputText.text = chatNewString;
+        //}
+    }
+}
---
+++ SpaceBattle/SpaceBattle.Core/SpaceBattle.Core/Scripts/ChatSystem/Message.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceBattle
+{
+    internal class Message
+    {
+        public string messageId { get; set; }
+        public string messageContent { get; set; }
+        public string messageUsername { get; set; }
+
+    }
+}
