5500d1611589f8d5e911fae27898821facec8df3
James Gefyrst <GnomenxD@gmail.com>
2022-10-13 13:56:12 +0200

    Update NetworkSpawner.cs

--- SpaceBattle/SpaceBattle.Core/SpaceBattle.Core/Scripts/NetworkSpawner.cs
+++ SpaceBattle/SpaceBattle.Core/SpaceBattle.Core/Scripts/NetworkSpawner.cs
@@ -10,24 +10,32 @@ namespace SpaceBattle
  		private uint netId = 255;
 		public void CreateNewPlayer(NetcodeClient newClient, NetcodeClient[] existingClients)
 		{
+			//We only call this method from the server.
 			if (!NetcodeHandler.IsServer)
 				return;
 
+			//Generate random position and unique netId
+			//NetID is actually not unique, this is a very crude way to do it. Works for now.
 			Vector2 position = new Vector2(Random.Range(-5, 5), Random.Range(-5, 5));
-
 			uint assignedId = netId++;
+
 			Debug.Log("Assigned ID: " + assignedId);
 			Debug.Log($"Create new player for {(newClient == null ? "null" : newClient)}");
 			if (newClient != null)
 			{
+				//Spawn a version of the new player ship on the server.
 				SpawnNewPlayer(assignedId, false, position);
+
+				//Spawn a player ship on the new client for each player ship already on the server.
 				foreach (NetcodeIdentity netObject in netcodeObjects)
 				{
 					Rpc(nameof(SpawnNewPlayer), newClient, netObject.NetId, false, netObject.Transform.Position);
 				}
 
+				//Spawn the new player's own ship.
 				Rpc(nameof(SpawnNewPlayer), newClient, assignedId, true, position);
 
+				//Last we send message to all already connected clients to spawn a new ship.
 				foreach (NetcodeClient client in existingClients)
 				{
 					Rpc(nameof(SpawnNewPlayer), client, assignedId, false, position);
